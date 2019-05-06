360ccdbc6ac83f7e0d5a1e151f32c1cbf963c60e
Alesh Houdek <mail@alesh.com>
2019-05-06 06:04:24 -0600

    Moved files into src folder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import store from './store'
+import Board from './components/Board';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-
+        <Board/>
       </div>
     </Provider>
   )
--- src/components/Board.js
+++ src/components/Board.js
@@ -4,7 +4,7 @@ export default class Board extends Component {
   render() {
     return (
       <div>
-
+        <h1>Board</h1>
       </div>
     )
   }
