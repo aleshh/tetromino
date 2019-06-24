f913cfda882729d7809a8dabdec49669e91a8a04
Alesh Houdek <mail@alesh.com>
2019-06-24 07:19:37 -0600

    Piece shows up on board

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,32 +1,60 @@
 import React, { Component } from 'react'
+import pieceLayouts from '../modules/piece/piece-layouts';
 
 export default class Board extends Component {
 
   buildBoard = () => {
     let tableRows = []
-    for (let i = 0; i < this.props.config.boardHeight; i++) {
-      tableRows.push(this.buildRow(i))
+    for (let row = 0; row < this.props.config.boardHeight; row++) {
+      tableRows.push(this.buildRow(row))
     }
     return (
         <tbody>{ tableRows }</tbody>
       )
   }
 
-  buildRow = rowId => {
+  buildRow = row => {
     let tableCells = []
-    for (let i = 0; i < this.props.config.boardWidth; i++) {
+    for (let column = 0; column < this.props.config.boardWidth; column++) {
+      const hasPiece = this.isPieceAt(row, column)
       tableCells.push(
-        <td key={i.toString()} id={ 'c-' + rowId + '-' + i } />
+        <td
+          key={column.toString()}
+          id={ 'c-' + row + '-' + column }
+          className={ hasPiece ? 'piece-' + this.currentPiece.type : null }
+        />
       )
     }
     return(
-      <tr key={ rowId.toString()} >
+      <tr key={ row.toString()} >
         { tableCells }
       </tr>
     )
   }
 
+  isPieceAt = (row, column) => {
+    // false if there's no piece
+    if (!this.currentPiece || !this.currentPiece.position) return false;
+
+    const pieceX = this.currentPiece.position[0]
+    const pieceY = this.currentPiece.position[1]
+
+    const x = column - pieceX
+    const y = row - pieceY
+
+    // false if the offset is greater than the piece size
+    if(!this.pieceLayout[y]) return false;
+
+    return this.pieceLayout[y][x]
+  }
+
   render() {
+    if (this.props.currentPiece.position) {
+      this.hasPiece = true
+      this.currentPiece = this.props.currentPiece
+      this.pieceLayout = pieceLayouts[this.currentPiece.type][this.currentPiece.rotation]
+    }
+
     return (
       <div>
         <table>
